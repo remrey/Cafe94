2163720ff08e2ee66fe97b2e7651ddd3db24f6ab
lorcanj <lorcan.james@yahoo.com>
2020-04-17 00:58:50 +0100

    Added JavaDoc

--- src/sample/ChefScreen/chefChangeItemController.java
+++ src/sample/ChefScreen/chefChangeItemController.java
@@ -1,6 +1,4 @@
 package sample.ChefScreen;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,16 +8,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import sample.DBManager;
-
 import java.sql.*;
-
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.ParsePosition;
 import java.util.regex.Pattern;
 
-public class chefChangeItemController {
-
+/**
+ * A controller for the screen for the chef to change the properties of an item.
+ * @author Lorcan, George
+ * @version 1.0
+ */
+public class ChefChangeItemController {
     @FXML private TextField itemID;
     @FXML private TextField itemName;
     @FXML private TextField itemPrice;
@@ -29,16 +27,16 @@ public class chefChangeItemController {
     @FXML private Label itemPriceError;
     @FXML private Label successLabel;
     @FXML private Label validIDError;
-    @FXML private Button changeButton;
-    @FXML private Button returnButton;
+    private Connection connection = null;
+    private ResultSet rs = null;
+    private PreparedStatement pst = null;
 
-
-
-    Connection connection = null;
-    ResultSet rs = null;
-    PreparedStatement pst = null;
-
-    private boolean isDecimal(String strNum){
+    /**
+     * A method to check whether a given input is a decimal.
+     * @param strNum the input entered by the chef
+     * @return whether the input is a decimal
+     */
+    private boolean isDecimal(final String strNum) {
         Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
         if (strNum == null) {
             return false;
@@ -46,22 +44,30 @@ public class chefChangeItemController {
         return pattern.matcher(strNum).matches();
     }
 
-    private boolean isInt(String text) {
+    /**
+     * A method that checks whether the input is an integer.
+     * @param text the input entered by the chef
+     * @return whether
+     */
+    private boolean isInt(final String text) {
         return text.matches("[0-9]*");
     }
 
-
-    public void onPressButton(ActionEvent event) throws IOException, SQLException {
+    /**
+     * A method that validates whether an input
+     * is valid or not and then updates the menu.
+     * @param event the button click of the user
+     * @throws IOException
+     * @throws SQLException
+     */
+    public void onPressButton(final ActionEvent event) throws IOException, SQLException {
         itemIDEmptyError.setVisible(false);
         itemNameEmptyError.setVisible(false);
         itemPriceError.setVisible(false);
         validIDError.setVisible(false);
         successLabel.setVisible(false);
-
         String query = "UPDATE menu SET name = ?, price = ?, type = ? WHERE id = ?;";
-
         connection = DBManager.DBConnection();
-
         if (!isInt(itemID.getText())) {
             validIDError.setText("Please enter a valid id");
             validIDError.setVisible(true);
@@ -72,8 +78,9 @@ public class chefChangeItemController {
             itemIDEmptyError.setText("This field cannot be left blank");
             itemIDEmptyError.setVisible(true);
         } else {
-            if (isInt(itemID.getText())){
-                if (Integer.parseInt(itemID.getText()) < 1 || Integer.parseInt(itemID.getText()) > 20){
+            if (isInt(itemID.getText())) {
+                if (Integer.parseInt(itemID.getText()) < 1 ||           // number 1 represents the lowest itemID in database
+                        Integer.parseInt(itemID.getText()) > 20) {       // number 20 represents highest itemID in the database
                     validIDError.setText("Please enter a valid id");
                     validIDError.setVisible(true);
                 } else {
@@ -81,30 +88,30 @@ public class chefChangeItemController {
                 }
             }
         }
-        if (itemName.getText().isEmpty()){
+        if (itemName.getText().isEmpty()) {
             itemNameEmptyError.setVisible(true);
         } else {
             itemNameEmptyError.setVisible(false);
         }
-        if (itemPrice.getText().isEmpty()){
+        if (itemPrice.getText().isEmpty()) {
             itemPriceError.setText("This field cannot be left blank");
             itemPriceError.setVisible(true);
         } else {
             itemPriceError.setVisible(false);
         }
-        if(!isDecimal(itemPrice.getText())){
+        if (!isDecimal(itemPrice.getText())) {
             itemPriceError.setText("Please enter numeric value");
             itemPriceError.setVisible(true);
         } else {
             itemPriceError.setVisible(false);
         }
-        if(!itemName.getText().isEmpty()
+        if (!itemName.getText().isEmpty()
                 && !itemID.getText().isEmpty()
                 && !itemPrice.getText().isEmpty()
                 && !itemType.getText().isEmpty()
                 && isInt(itemID.getText())
                 && isDecimal(itemPrice.getText())
-                && Integer.parseInt(itemID.getText()) > 0
+                && Integer.parseInt(itemID.getText()) > 0       // checks whether itemID is valid between 0 and 20
                 && Integer.parseInt(itemID.getText()) < 21
         ) {
             try {
@@ -114,28 +121,27 @@ public class chefChangeItemController {
                 pst.setString(3, itemType.getText());
                 pst.setInt(4, Integer.parseInt(itemID.getText()));
                 pst.executeUpdate();
-                successLabel.setText(itemName.getText() + " has been updated on the system");
+                successLabel.setText(itemName.getText()
+                        + " has been updated on the system");
                 successLabel.setVisible(true);
-
-            }
-            catch (SQLException e){
+            } catch (SQLException e) {
                 e.printStackTrace();
-            }
-            catch (Exception ex) {
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
-
-    public void returnButtonPushed(ActionEvent event) throws IOException {
+    /**
+     * A method to return to the previous screen.
+     * @param event button click that returns user to main screen
+     * @throws IOException
+     */
+    public void returnButtonPushed(final ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("/sample/ChefScreen/chefMainScreen.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
-
         //This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(tableViewScene);
         window.show();
     }
-
 }
