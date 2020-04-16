a078b3ded3fc5f2d7737c01ca626a3f76ffba74d
lorcanj <lorcan.james@yahoo.com>
2020-04-16 20:00:08 +0100

    Update Booking.java

--- src/sample/Booking.java
+++ src/sample/Booking.java
@@ -9,21 +9,79 @@ import javafx.fxml.FXML;
 public class Booking {
 
     private final SimpleIntegerProperty bookingID;
-    private final SimpleStringProperty customerID;
-    private final SimpleStringProperty time;
+    private final SimpleIntegerProperty customerID;
+    private final SimpleStringProperty timePeriod;
     private final SimpleStringProperty date;
     private final SimpleBooleanProperty extended;
     private final SimpleIntegerProperty numberOfGuests;
-    private boolean approved;
+    private final SimpleBooleanProperty approved;
 
-    public Booking(int bookingID, String date, String time, int numberOfGuests, Boolean extended, String customerID){
+    public Booking(int bookingID, String date, String timePeriod, int numberOfGuests, Boolean extended, int customerID){
         this.bookingID = new SimpleIntegerProperty(bookingID);
-        this.customerID = new SimpleStringProperty(customerID);
-        this.time = new SimpleStringProperty(time);
+        this.customerID = new SimpleIntegerProperty(customerID);
+        this.timePeriod = new SimpleStringProperty(timePeriod);
         this.date = new SimpleStringProperty(date);
         this.extended = new SimpleBooleanProperty(extended);
         this.numberOfGuests = new SimpleIntegerProperty(numberOfGuests);
-        this.approved = false;
+        this.approved = new SimpleBooleanProperty(false);
+    }
+
+    public Booking(){
+        this.bookingID = new SimpleIntegerProperty(0);
+        this.customerID = new SimpleIntegerProperty(0);
+        this.timePeriod = new SimpleStringProperty("zero");
+        this.date = new SimpleStringProperty("00-00-00");
+        this.extended = new SimpleBooleanProperty(false);
+        this.numberOfGuests = new SimpleIntegerProperty(0);
+        this.approved = new SimpleBooleanProperty(false);
+    }
+
+    public String getTimePeriod() {
+        return timePeriod.get();
+    }
+
+    public SimpleStringProperty timePeriodProperty() {
+        return timePeriod;
+    }
+
+    public void setTimePeriod(String timePeriod) {
+        this.timePeriod.set(timePeriod);
+    }
+
+    public int getCustomerID() {
+        return customerID.get();
+    }
+
+    public SimpleIntegerProperty customerIDProperty() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID.set(customerID);
+    }
+
+    public boolean isExtended() {
+        return extended.get();
+    }
+
+    public SimpleBooleanProperty extendedProperty() {
+        return extended;
+    }
+
+    public void setExtended(boolean extended) {
+        this.extended.set(extended);
+    }
+
+    public boolean isApproved() {
+        return approved.get();
+    }
+
+    public SimpleBooleanProperty approvedProperty() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved.set(approved);
     }
 
     public Boolean getExtended() {
@@ -42,14 +100,6 @@ public class Booking {
         this.numberOfGuests.set(numberOfGuests);
     }
 
-    public boolean isApproved() {
-        return approved;
-    }
-
-    public void setApproved(boolean approved) {
-        this.approved = approved;
-    }
-
     public int getBookingID() {
         return bookingID.get();
     }
@@ -60,30 +110,6 @@ public class Booking {
 
     public void setBookingID(int bookingID) {
         this.bookingID.set(bookingID);
-    }
-
-    public String getCustomerID() {
-        return customerID.get();
-    }
-
-    public SimpleStringProperty customerIDProperty() {
-        return customerID;
-    }
-
-    public void setCustomerID(String customerID) {
-        this.customerID.set(customerID);
-    }
-
-    public String getTime() {
-        return time.get();
-    }
-
-    public SimpleStringProperty timeProperty() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time.set(time);
     }
 
     public String getDate() {
