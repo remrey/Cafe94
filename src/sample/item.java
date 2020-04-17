bc105930d9de72da5ed4351cc611a5ecba7f8086
lorcanj <lorcan.james@yahoo.com>
2020-04-17 01:43:11 +0100

    Added JavaDoc comments

--- src/sample/item.java
+++ src/sample/item.java
@@ -5,6 +5,12 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Stores information regarding an item on the menu.
+ * @author Emre.
+ * @version 1.0
+ * @param <SimpleDoublePropertyrice>
+ */
 public class item<SimpleDoublePropertyrice> {
     private SimpleIntegerProperty id;
     private SimpleStringProperty itemName;
@@ -12,7 +18,16 @@ public class item<SimpleDoublePropertyrice> {
     private SimpleBooleanProperty isDailySpecial;
     private SimpleStringProperty type;
 
-    public item(SimpleIntegerProperty id, SimpleStringProperty itemName, SimpleDoubleProperty price, SimpleBooleanProperty isDailySpecial, SimpleStringProperty type) {
+    /**
+     * Creates a menu item with the arguments passed to the method.
+     * @param id The id of the menu item.
+     * @param itemName The name of the menu item.
+     * @param price The price of the menu item.
+     * @param isDailySpecial States whether the item is the daily special.
+     * @param type Determines the course type of the menu item.
+     */
+    public item(final SimpleIntegerProperty id, final SimpleStringProperty itemName, final SimpleDoubleProperty price,
+                final SimpleBooleanProperty isDailySpecial, final SimpleStringProperty type) {
         this.id = id;
         this.itemName = itemName;
         this.price = price;
@@ -20,6 +35,9 @@ public class item<SimpleDoublePropertyrice> {
         this.type = type;
     }
 
+    /**
+     * A default constructor to create a menu item.
+     */
     public item(){
         this.id = new SimpleIntegerProperty(0);
         this.itemName = new SimpleStringProperty("zero");
@@ -28,66 +46,130 @@ public class item<SimpleDoublePropertyrice> {
         this.type = new SimpleStringProperty("zero");
     }
 
+    /**
+     * Returns the course type of the menu item.
+     * @return Returns the course type of the menu item.
+     */
     public String getType() {
         return type.get();
     }
 
+    /**
+     * Returns the course type of the menu item.
+     * @return Returns the course type of the menu item.
+     */
     public SimpleStringProperty typeProperty() {
         return type;
     }
 
-    public void setType(String type) {
+    /**
+     * Sets the course type of the menu item.
+     * @param type Sets the course type of the menu item.
+     */
+    public void setType(final String type) {
         this.type.set(type);
     }
 
+    /**
+     * Returns the id of the menu item.
+     * @return Returns the id of the menu item.
+     */
     public int getId() {
         return id.get();
     }
 
+    /**
+     * Returns the id of the menu item.
+     * @return Returns the id of the menu item.
+     */
     public SimpleIntegerProperty idProperty() {
         return id;
     }
 
-    public void setId(int id) {
+    /**
+     * Sets the id of the menu item.
+     * @param id Sets the id of the menu item.
+     */
+    public void setId(final int id) {
         this.id.set(id);
     }
 
+    /**
+     * Returns the item name of the menu item.
+     * @return Returns the item name of the menu item.
+     */
     public String getItemName() {
         return itemName.get();
     }
 
+    /**
+     * Returns the item name of the menu item.
+     * @return Returns the item name of the menu item.
+     */
     public SimpleStringProperty itemNameProperty() {
         return itemName;
     }
 
-    public void setItemName(String itemName) {
+    /**
+     * Sets the item name of the menu item.
+     * @param itemName Sets the item name of the menu item.
+     */
+    public void setItemName(final String itemName) {
         this.itemName.set(itemName);
     }
 
+    /**
+     * Returns the price of the menu item.
+     * @return Returns the price of the menu item.
+     */
     public double getPrice() {
         return price.get();
     }
 
+    /**
+     * Returns the price of the menu item.
+     * @return Returns the price of the menu item.
+     */
     public SimpleDoubleProperty priceProperty() {
         return price;
     }
 
-    public void setPrice(double price) {
+    /**
+     * Sets the price of the menu item.
+     * @param price Sets the price of the menu item.
+     */
+    public void setPrice(final double price) {
         this.price.set(price);
     }
 
+    /**
+     * Returns whether the menu item is the dailySpecial.
+     * @return Returns whether the menu item is the dailySpecial.
+     */
     public boolean isIsDailySpecial() {
         return isDailySpecial.get();
     }
 
+    /**
+     * Returns whether the menu item is the dailySpecial.
+     * @return Returns whether the menu item is the dailySpecial.
+     */
     public SimpleBooleanProperty isDailySpecialProperty() {
         return isDailySpecial;
     }
 
-    public void setIsDailySpecial(boolean isDailySpecial) {
+    /**
+     * Sets whether the menu item is the dailySpecial.
+     * @param isDailySpecial Sets whether the menu item is the dailySpecial.
+     */
+    public void setIsDailySpecial(final boolean isDailySpecial) {
         this.isDailySpecial.set(isDailySpecial);
     }
 
+    /**
+     * Returns the item name of the menu item.
+     * @return Returns the item name of the menu item.
+     */
     @Override
     public String toString() {
         return this.getItemName();
